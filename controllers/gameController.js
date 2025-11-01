50f99d1e3cde151766e5794a33262a6ba7eb6fca
Raihan Sharif <raihansharif92@gmail.com>
2025-11-01 21:07:51 +0000

    correctly formatted date and can get game list filtered by studio

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -2,7 +2,11 @@ const { body, validationResult, matchedData } = require("express-validator");
 const db = require("../db/queries");
 
 async function getGames(req, res) {
-  const gameData = await db.getAllGames();
+  const gameData = await db.gamesFilteredByStudio([
+    "CD Projekt Red",
+    "Valve Corporation",
+  ]);
+  // const gameData = await db.getAllGames();
   console.log(gameData);
   res.json(gameData);
 }
--- db/queries.js
+++ db/queries.js
@@ -1,8 +1,19 @@
 const pool = require("./pool");
 
+async function gamesFilteredByStudio(studioArr) {
+  const result = await pool.query(
+    "SELECT title, TO_CHAR(publish_date, 'YYYY-MM-DD') as publish_date, rating \
+    FROM game JOIN studio ON game.studio_id = studio.id \
+    WHERE studio.name = ANY($1)",
+    [studioArr]
+  );
+  const { rows } = result;
+  return rows;
+}
+
 async function getAllGames() {
   const result = await pool.query(
-    "SELECT title, (publish_date, 'YYYY-DD-MM') as publish_date, rating FROM game \
+    "SELECT title, TO_CHAR(publish_date, 'YYYY-MM-DD') as publish_date, rating FROM game \
     WHERE title IN ('Dota 2', 'Minecraft')"
   );
   const { rows } = result;
@@ -17,5 +28,6 @@ async function getAllStudios() {
 
 module.exports = {
   getAllGames,
+  gamesFilteredByStudio,
   getAllStudios,
 };
