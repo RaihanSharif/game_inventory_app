6a6087e8ecd298183bcdea37480b25475e789766
Raihan Sharif <raihansharif92@gmail.com>
2025-11-02 17:50:49 +0000

    show unfiltered list of games and some layouts

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -2,18 +2,15 @@ const { body, validationResult, matchedData } = require("express-validator");
 const db = require("../db/queries");
 
 async function getGames(req, res) {
-  const gameData = await db.getGamesList(
-    ["First Person Shooter", "Open World"],
-    ["Valve Corporation"]
-  );
+  const gameData = await db.getGamesList();
   // figuring out query strings, ?studio=Valve+Corporation&studio=Mojang+Studios&genre=First+Person+Shooter
   // outout in req.query is:
   //   [Object: null prototype] {
   //   studio: [ 'Valve Corporation', 'Mojang Studios' ],
   //   genre: 'First Person Shooter'
   // }
-  console.log(gameData);
   console.log(req.query);
+  console.log(gameData);
   res.render("index", { title: "List of games", games: gameData });
 }
 
--- db/queries.js
+++ db/queries.js
@@ -36,22 +36,24 @@ async function gamesFilteredByGenreAndStudio(genreArr, studioArr) {
   return rows;
 }
 
-async function getGamesList(genreArr = null, studioArr = null) {
-  if (genreArr !== null && studioArr !== null) {
-    return gamesFilteredByGenreAndStudio(genreArr, studioArr);
-  }
-  if (genreArr !== null) {
-    return gamesFilteredByGenre(genreArr);
-  }
-  if (studioArr !== null) {
-    return gamesFilteredByStudio(studioArr);
-  }
-
-  const result = pool.query(
-    "SELECT id, title, TO_CHAR(publish_date, 'YYYY-MM-DD') as publish_date, rating FROM game"
+async function getAllGames() {
+  const result = await pool.query(
+    "SELECT game.id, title, TO_CHAR(publish_date, 'YYYY-MM-DD') as publish_date, rating FROM game;"
   );
   const { rows } = result;
   return rows;
+}
+
+async function getGamesList(genreArr, studioArr) {
+  if (genreArr && studioArr) {
+    return gamesFilteredByGenreAndStudio(genreArr, studioArr);
+  } else if (genreArr) {
+    return gamesFilteredByGenre(genreArr);
+  } else if (studioArr) {
+    return gamesFilteredByStudio(studioArr);
+  } else {
+    return getAllGames();
+  }
 }
 
 async function getAllStudios() {
