b829aad4a80114b4f4dbb0cc9fd1e74c0b1e1a4a
Raihan Sharif <raihansharif92@gmail.com>
2025-11-03 14:06:43 +0000

    trying to connect with SSL on render

--- db/pool.js
+++ db/pool.js
@@ -1,3 +1,8 @@
 const { Pool } = require("pg");
 
-module.exports = new Pool();
+module.exports = new Pool({
+  connectionString: process.env.PGDATABASE,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
